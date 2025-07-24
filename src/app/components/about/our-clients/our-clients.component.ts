720d661e0cf6307d88e471d7558a88ca399360f7
Teja Venkata Mani <manivenkatateja2000.eemani@gmail.com>
2025-07-24 15:51:46 +0530

    final commit

--- src/app/components/about/our-clients/our-clients.component.ts
+++ src/app/components/about/our-clients/our-clients.component.ts
@@ -12,35 +12,35 @@ export class OurClientsComponent {
   partners = [
     {
       name: 'Tesync',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZD8pwPqGHSWVDxv-JqaFkXwc2nRjXTIRqwQ&s'
+      image: 'assets/client-logos/tesync.jpg'
     },
     {
       name: 'Robo soft',
-      image: 'https://www.topdevelopers.co/upload/20190211103124611698604.jpg'
+      image: 'assets/client-logos/robosoft.jpg'
     },
     {
       name: 'ahana',
-      image: 'https://ahanait.com/wp-content/uploads/2024/02/New1-Ahana-2024-website-Logo-Medium.svg'
+      image: 'assets/client-logos/ahana.jpg'
     },
     {
       name: 'Technogen',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZRZ-4dmMX1cr55KMmnZ0HG_xo41Fe7_R7xrTUraV8lkDQRkEtgsir8f_L5Dr8wakBWA&usqp=CAU'
+      image: 'assets/client-logos/technogen.jpg'
     },
     {
       name: 'Multiverse Solutions',
-      image: 'https://media.licdn.com/dms/image/v2/C4D0BAQFLPPAY1STFuQ/company-logo_200_200/company-logo_200_200/0/1675362164060/multiversesolutions_logo?e=2147483647&v=beta&t=_vV2flTJ0niCyjoJju_5arQ2XBEjNE2gKGa1OffL5Q4'
+      image: 'assets/client-logos/multiverse.jpg'
     },
     {
       name: 'SBIOA',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJECm7dGZo91pIL9jDYWVLN7tRobdAzLloSxhaoL1HjW2Ch-oe06IPQS3138YGPGrP_pA&usqp=CAU'
+      image: 'assets/client-logos/sbi.jpg'
     },
     {
       name: 'Pi',
-      image: 'https://www.equitypandit.com/wp-content/uploads/2023/08/pi-industries-ltd-2.png'
+      image: 'assets/client-logos/pi.jpg'
     },
     {
       name: 'Tgsrtc',
-      image: 'https://th-i.thgim.com/public/migration_catalog/article10538854.ece/alternates/FREE_1200/13hyskm06-TSRTChy14TSRTC-emblem.jp.jpg'
+      image: 'assets/client-logos/tgsrtc.jpg'
     }
   ];
 }
